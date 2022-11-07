942d305ad42e9ed2192c796649302662d071181c
hannatrush <h4annatrush@gmail.com>
2022-11-07 10:44:29 +0200

    Network request and global store added

--- src/components/PetBlock.jsx
+++ src/components/PetBlock.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import PetModal from './PetModal';
 
-function PetBlock({imageUrl, name}) {
+function PetBlock({imageUrl, name, age, type}) {
   const [modalActive, setModalActive] = React.useState(false);
 
     return (
         <div className="pets-item">
           <img className="main_img" src={imageUrl} alt=""/>
           <p className="name">{name}</p>
-          <a onClick={() => setModalActive(true)} href="#" className="button">Learn more</a>
+          <a onClick={(e) =>
+           {e.preventDefault();
+           setModalActive(true);}} href="#" className="button">Learn more</a>
           {
-            modalActive && <PetModal modalActive={modalActive} setModalActive={setModalActive}/>
+            modalActive && <PetModal imageUrl={imageUrl} name={name} age={age} type={type} setModalActive={setModalActive}/>
           }
         </div>
     )
--- src/components/PetModal.jsx
+++ src/components/PetModal.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 
-function PetModal({modalActive, setModalActive}) {
+function PetModal({name, imageUrl, age, type, setModalActive}) {
    return(
-    <div className="pet-modal">
-        <button className="close" onClick={() =>setModalActive(false)}>
-        <svg className="x" data-name="Layer 1" height="200" id="Layer_1" viewBox="0 0 200 200" width="200"><title/><path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/></svg>
-        </button>
-        <img className="pet" src="img/2dog.png" alt=""/>
-        <div className="modal-text">
-            <h3 className="pet-name">Jennifer</h3>
-            <h4 className="type">Pooch + Labrador</h4>
-            <div className="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce <br/> efficitur blandit condimentum. Proin accumsan lorem vel gra <br/>vida fringilla. Suspendisse potenti. Mauris ut pulvinar nunc. <br/>Donec consectetur, diam in porta tempus, urna ligula ves <br/>tibulum nibh.
+    <div className='modal-wrapper'>
+        <div className="pet-modal">
+            <button className="close" onClick={() =>setModalActive(false)}>
+            <svg className="x" data-name="Layer 1" height="200" id="Layer_1" viewBox="0 0 200 200" width="200"><title/><path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/></svg>
+            </button>
+            <img className="pet" src={imageUrl} alt=""/>
+            <div className="modal-text">
+                <h3 className="pet-name">{name}</h3>
+                <h4 className="type">{type}</h4>
+                <div className="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce <br/> efficitur blandit condimentum. Proin accumsan lorem vel gra <br/>vida fringilla. Suspendisse potenti. Mauris ut pulvinar nunc. <br/>Donec consectetur, diam in porta tempus, urna ligula ves <br/>tibulum nibh.
+                </div>
+                <ul>
+                    <li><span className="info">Age: </span><span className="info-text" >{age}</span></li>
+                    <li><span className="info">Inoculation: </span><span className="info-text" >none</span></li>
+                    <li><span className="info">Diseases: </span><span className="info-text" >none</span></li>
+                    <li><span className="info">Parasites: </span><span className="info-text" >none</span></li>
+                </ul>
             </div>
-            <ul>
-                <li><span className="info">Age: </span><span className="info-text" >2 month</span></li>
-                <li><span className="info">Inoculation: </span><span className="info-text" >none</span></li>
-                <li><span className="info">Diseases: </span><span className="info-text" >none</span></li>
-                <li><span className="info">Parasites: </span><span className="info-text" >none</span></li>
-            </ul>
         </div>
     </div>
    ) 
